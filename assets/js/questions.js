10093dbdffb7c9144375c4d05f9fb89d1aa71a0b
logarythm <mahmood.ihsan98989@gmail.com>
2023-01-22 21:45:07 +0000

    Added question objects to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,62 @@
 var questions = [
     {
-        question: "";
-        choice1 : "";
-        choice2 : "";
-        choice3 : "";
-        choice4 : "";
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1 : "<scripting>",
+        choice2 : "<javascript>",
+        choice3 : "<script>",
+        choice4 : "<js>",
+        answer : "<script>"
+    },
+    {
+        question: "The external JavaScript file must contain the <script> tag",
+        choice1 : "True",
+        choice2 : "False",
+        answer : "True"
+    },
+    {
+        question: "How would you create a function in JavaScript? ",
+        choice1 : "function myfunction()",
+        choice2 : "function = myfunction()",
+        choice3 : "function:myfunction()",
+        answer : "function myfunction()"
+    },
+    {
+        question: "What must string values be enclosed in when assigned to variables",
+        choice1 : "commas",
+        choice2 : "quotes",
+        choice3 : "curly brackets",
+        choice4 : "parenthesis",
+        answer : "quotes"
+    },
+    {
+        question: "commonly used data types do not include:",
+        choice1 : "strings",
+        choice2 : "booleans",
+        choice3 : "alerts",
+        choice4 : "numbers",
+        answer : "alerts"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choice1 : "onclick",
+        choice2 : "onchange",
+        choice3 : "mouseclick",
+        choice4 : "clickinput",
+        answer : "onclick"
+    },
+    {
+        question: "arrays are zero-indexed",
+        choice1 : "true",
+        choice2 : "false",
+        answer : "true"
+    },
+    {
+        question: "A very useful tool which is used during debugging and development for printing content to the debugger is",
+        choice1 : "JavaScript",
+        choice2 : "Terminal",
+        choice3 : "for loops",
+        choice4 : "console.log",
+        answer : "console.log"
     }
+
 ]
