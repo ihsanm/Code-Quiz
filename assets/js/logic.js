efbb7aa99c0b28ad55b2139124eeb27fd6615f91
logarythm <mahmood.ihsan98989@gmail.com>
2023-01-23 13:05:38 +0000

    added code to hide start screen and show questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,7 +2,8 @@
 var sfxright = new Audio("assets/sfx/correct.wav");
 var sfxwrong = new Audio("assets/sfx/incorrect.wav");
 
-var startquizEL = document.getElementById("start-quiz");
+var startquizEL = document.getElementById("start-screen");
+var startbutton = document.querySelector("#start");
 var questionsEL = document.getElementById("questions");
 var choicesEL = document.getElementById("choices");
 var endscreen = document.getElementById("end-screen");
@@ -10,3 +11,15 @@ var initials = document.getElementById("initials")
 var submit = document.getElementById("submit");
 var questionIndex = 1;
 var timeleft = 130;
+
+
+startbutton.addEventListener("click", function(){
+    startquizEL.classList.remove("start");
+    startquizEL.classList.add("hide");
+    questionsEL.classList.remove("hide");
+    questionsEL.classList.add("start");
+    startquizEL.style.display("none")
+});
+
+
+
